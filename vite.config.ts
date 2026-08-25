260e5060ca8aa6d01bc0cbcfc71623d2724ccdae
孙盼盼 <sunpanpan@sunpanpandeMacBook-Pro.local>
2026-08-25 16:09:56 +0800

    update base path to /work-dashboad/

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/work-dashboad/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
